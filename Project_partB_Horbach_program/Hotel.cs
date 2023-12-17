5341ddf04815b0100052fe0daf105820b75c0397
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-12-17 20:31:25 +0100

    Реалізація Program

--- Project_partB_Horbach_program/Hotel.cs
+++ Project_partB_Horbach_program/Hotel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project_partB_Horbach_program;
 
 namespace Project_partB_Horbach_program
 {
