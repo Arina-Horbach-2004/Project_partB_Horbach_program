b703cf53de3fea9592cd89f2998fca3231db483b
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-12-17 19:59:51 +0100

    Добавьте файлы проекта.

---
+++ Project_partB_Horbach_program/RoomType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partB_Horbach_program
+{
+    public enum RoomType
+    {
+        Standard,
+        Luxury,
+        Familyroom,
+        Oceanview,
+        BusinessClass,
+        Villa,
+        Longstay,
+        Single,
+        Double,
+        Withseparatebeds
+    }
+}
---
+++ Project_partB_Horbach_program/StaffPossition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partB_Horbach_program
+{
+    public enum StaffPosition
+    {
+        Receptionist,
+        Administrator,
+        Cleaner,
+        Staff,
+        Manager,
+        Concierge,
+        RegistrationManager,
+        FrontDeskClerk,
+
+    }
+}
