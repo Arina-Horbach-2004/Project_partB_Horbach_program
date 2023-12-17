67fd04cac2a05b57cf4e74db95f216d61b1bd449
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-12-17 20:05:13 +0100

    Реалізація інтерфейсу IHotel

--- Project_partB_Horbach_program/IHotel.cs.cs
+++ Project_partB_Horbach_program/IHotel.cs.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Project_partB_Horbach_program
 {
-    internal interface IHotel
+    public interface IHotel
     {
+        // Реєстрація персоналу
+        public void RegisterStaff(HotelStaff staff);
+        // Виселення посетителя
+        public void RemoveStaff(HotelStaff staffm);
+
+        // Метод для реєстрації гостя в готелі
+        public void RegisterGuest(Guest guest, HotelStaff checkedInBy, Room room, int stayDuration);
+
+        // Метод для виписування гостя з готелю
+        public void RemoveGuest(Guest guest, HotelStaff checkedOutBy, Room room);
+
+        // Метод для отримання списку персоналу готелю
+        public List<HotelStaff> GetStaffList();
+
+        // Метод для отримання списку гостей готелю
+        public List<Guest> GetGuestList();
+
+        // Метод для отримання списку всіх кімнат готелю
+        public List<Room> GetRoomList();
+
+        // Метод для отримання списку заселених гостей готелю
+        public List<Guest> GetCheckedInGuests();
+
+        // Метод для отримання списку виписаних гостей готелю
+        public List<Guest> GetCheckedOutGuests();
+
+        public string ToString();
     }
 }
