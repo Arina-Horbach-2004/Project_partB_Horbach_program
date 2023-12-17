664c08c20c8b0463e4a843fb2c06d9b33541febc
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-12-17 20:04:26 +0100

    Реалізація класу HotelStaff

--- Project_partB_Horbach_program/HotelStaff.cs
+++ Project_partB_Horbach_program/HotelStaff.cs
@@ -6,7 +6,65 @@ using System.Threading.Tasks;
 
 namespace Project_partB_Horbach_program
 {
-    internal class HotelStaff
+    public class HotelStaff : PersonBase
     {
+        private static int idCounter = 0;
+
+        public virtual string FirstName
+        {
+            get => base.FirstName;
+            set => base.FirstName = value;
+        }
+
+        public virtual string LastName
+        {
+            get => base.LastName;
+            set => base.LastName = value;
+        }
+
+        public virtual string ContactNumber
+        {
+            get => base.ContactNumber;
+            set => base.ContactNumber = value;
+        }
+
+        public StaffPosition StaffPosition { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsDismissed { get; set; }
+
+        public int Id { get; set; }
+
+        public HotelStaff(string firstName, string lastname, string contactNumber, DateTime birthDate, StaffPosition position)
+        {
+            idCounter++;
+            Id = idCounter;
+            FirstName = firstName;
+            LastName = lastname;
+            ContactNumber = contactNumber;
+            BirthDate = birthDate;
+            StaffPosition = position;
+        }
+
+        public string Get_Full_Name()
+        {
+            return $"{FirstName} {LastName}".Trim();
+        }
+
+        public string GetContactNumber()
+        {
+            return ContactNumber;
+        }
+
+        public string ToStringAcctiveStaff()
+        {
+            return $" Staff Id: {Id}, Name: {Get_Full_Name()}, Contact Number: {ContactNumber}, Birthdate: {BirthDate.ToShortDateString()}, Position: {StaffPosition}";
+        }
+
+        public string ToStringDismissed()
+        {
+            return $" Dismissed Staff Id: {Id}, Name: {Get_Full_Name()}, Contact Number: {ContactNumber}, Birthdate: {BirthDate.ToShortDateString()}, Position: {StaffPosition}";
+        }
     }
 }
