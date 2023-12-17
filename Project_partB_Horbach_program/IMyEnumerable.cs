03f71ff44f38a8f9a3938f264a993074a5c058c4
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-12-17 20:22:39 +0100

    Створення та реалізація інтерфейсу IMyEnumerable

---
+++ Project_partB_Horbach_program/IMyEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_partB_Horbach_program
+{
+    public interface IMyEnumerable
+    {
+        IEnumerator GetEnumerator();
+    }
+}
